d3a5b28ff9b84bb28343c4bb16e55d8d36270f34
SpandanaKonda <kondaspandana6.6@gmail.com>
2025-07-31 14:52:04 +0530

    SCM commmit

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ class Sample{
 	public static void main(String [] args){
 		System.out.println("hello World");
 		System.out.println("Welcome to jenkins");
+		System.out.println("SCM polling to monitor changes in github");
 		
 	}
 }
